4a32b273282d9fa2c4c83823722e354ea6c48258
timhdang <71614530+timhdang@users.noreply.github.com>
2022-09-20 01:06:07 -0700

    Update scraper.py
    Update/Fix bugs on September 19th

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import wget
 import tarfile
 from os.path import exists
 import time
-import scannerX
+import scanner
 import os
 
 def main():
@@ -14,7 +14,7 @@ def main():
     driver.get("https://pypi.org/search/?q=&o=-created&c=Programming+Language+%3A%3A+JavaScript")
     driver.implicitly_wait(3)
     package = driver.find_elements(By.CSS_SELECTOR, 'ul.unstyled>li:nth-child(n)>a')
-    for i in range(2):
+    for i in range(30):
         print(package[i].get_attribute('href'))
         print("Download files of project" + package[i].text)
         title = package[i].text
@@ -25,10 +25,12 @@ def main():
         print("url=" + url_download.get_attribute('href'))
         #print(url_download.text)
         #url_download.click()
+        
         path = "C:/Users/17142/Desktop/Cyber/Package/"
         file_name = driver2.find_element(By.CSS_SELECTOR, '#content > div.banner > div > div.package-header__left > h1').text
-        ext = file_name[-4:]
+        ext = url_download.text[-4:]
         file_name_no_ext = file_name
+        print("FILE EXT =" + ext)
         if (ext == ".tgz"):
             file_name_no_ext = title.replace(".tgz","")
         elif (ext == "r.gz"):
@@ -36,18 +38,16 @@ def main():
         else: 
             file_name_no_ext = file_name
         print('file_name_no_ext =' + file_name_no_ext)
-        file_exists = exists(file_name)
-        os.mkdir(path+ str(file_name_no_ext))
+        file_exists = exists(file_name+ ext)
         if (file_exists):
-            print("file already exists")
+                print("file already exists")
         else:
             wget.download(url_download.get_attribute('href'),path)
             time.sleep(3)
             driver.implicitly_wait(3)
             print('filename=' + file_name)
             # open file
-            if (file_exists):
-                
+            if (file_exists):             
                 if ('whl' in file_name):
                     print('Skipped .whl files')
                 elif ('gzip' in file_name):
@@ -58,6 +58,7 @@ def main():
                     print('Skipped .lzma files')
                 else:
                     print('UNZIPPING A TAR file')
+                    print(path)
                     print("CHECKPOINT : " + file_name_no_ext  + "/" + file_name)
                     print("path + filename=" + path + file_name)
                     file = tarfile.open(path + file_name)
@@ -69,13 +70,14 @@ def main():
                 print("path + filename=" + path + file_name)
                 print("CHECKPOINT : " + file_name_no_ext + "/" + file_name)
                 print('UNZIPPING A TAR file AFTER WAITED 10 S')
-                file = tarfile.open(path + file_name)
+                file = tarfile.open(path + url_download.text)
                 # extracting file
-                file.extractall(path+ str(file_name_no_ext))
+                file.extractall(path)
                 file.close()
+        
 
-
+#
 if __name__ == '__main__':
     main() 
-    scannerX.main()
+    scanner.main()
         
