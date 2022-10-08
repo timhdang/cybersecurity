0e4502fbd24331b784a1226356cc3e9e034a64e6
timhdang <71614530+timhdang@users.noreply.github.com>
2022-10-07 17:02:43 -0700

    Update scraper.py on 10/07

--- scraper.py
+++ scraper.py
@@ -4,8 +4,8 @@ import wget
 import tarfile
 from os.path import exists
 import time
-import scanner
-import os
+import pathlib
+
 
 def main():
     options = webdriver.ChromeOptions()
@@ -14,30 +14,24 @@ def main():
     driver.get("https://pypi.org/search/?q=&o=-created&c=Programming+Language+%3A%3A+JavaScript")
     driver.implicitly_wait(3)
     package = driver.find_elements(By.CSS_SELECTOR, 'ul.unstyled>li:nth-child(n)>a')
-    for i in range(30):
-        print(package[i].get_attribute('href'))
-        print("Download files of project" + package[i].text)
+    package_per_page_upper_limit = len(package)
+    path = str(pathlib.Path().resolve()) + "\Package\\" 
+    for i in range(package_per_page_upper_limit):
         title = package[i].text
         driver2 = webdriver.Chrome(options=options)
-        print(package[i].get_attribute('href')+'#files')
         driver2.get(package[i].get_attribute('href')+'#files')
         url_download =  driver2.find_element(By.CSS_SELECTOR,'#files > div:nth-child(4) > div.card.file__card > a:nth-child(1)')
-        print("url=" + url_download.get_attribute('href'))
-        #print(url_download.text)
-        #url_download.click()
-        
-        path = "C:/Users/17142/Desktop/Cyber/Package/"
         file_name = driver2.find_element(By.CSS_SELECTOR, '#content > div.banner > div > div.package-header__left > h1').text
         ext = url_download.text[-4:]
         file_name_no_ext = file_name
-        print("FILE EXT =" + ext)
+        #print("FILE EXT =" + ext)
         if (ext == ".tgz"):
             file_name_no_ext = title.replace(".tgz","")
         elif (ext == "r.gz"):
             file_name_no_ext = title.replace(".tar.gz","")
         else: 
             file_name_no_ext = file_name
-        print('file_name_no_ext =' + file_name_no_ext)
+        #print('file_name_no_ext =' + file_name_no_ext)
         file_exists = exists(file_name+ ext)
         if (file_exists):
                 print("file already exists")
@@ -57,27 +51,18 @@ def main():
                 elif ('lzma' in file_name):
                     print('Skipped .lzma files')
                 else:
-                    print('UNZIPPING A TAR file')
-                    print(path)
-                    print("CHECKPOINT : " + file_name_no_ext  + "/" + file_name)
-                    print("path + filename=" + path + file_name)
                     file = tarfile.open(path + file_name)
                     # extracting file
                     file.extractall(path + str(file_name_no_ext))
                     file.close()
             else:
-                time.sleep(10)
-                print("path + filename=" + path + file_name)
-                print("CHECKPOINT : " + file_name_no_ext + "/" + file_name)
+                time.sleep(10) 
                 print('UNZIPPING A TAR file AFTER WAITED 10 S')
                 file = tarfile.open(path + url_download.text)
                 # extracting file
                 file.extractall(path)
                 file.close()
-        
-
-#
 if __name__ == '__main__':
     main() 
-    scanner.main()
+    
         
